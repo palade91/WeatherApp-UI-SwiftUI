fc55baf4c901ac9fa39888e4cef5b727a679fa34
Catalin Palade <catalin@recruitd.com>
2020-12-21 15:09:13 +0000

    Initial Commit

---
+++ WeatherApp-UI-SwiftUI/WeatherApp_UI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_UI_SwiftUIApp.swift
+//  WeatherApp-UI-SwiftUI
+//
+//  Created by Catalin Palade on 21/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_UI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
