2a3c3b2bcbacacbd5f7e513c9c56d65bec542ee7
Catalin Palade <catalin@recruitd.com>
2020-12-21 16:17:10 +0000

    starting adding UI, city name, temperature

--- WeatherApp-UI-SwiftUI/ContentView.swift
+++ WeatherApp-UI-SwiftUI/ContentView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Manchester, UK")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack(spacing: 10) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("17°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                
+                    
+                Spacer()
+            }
+        }
     }
 }
 
